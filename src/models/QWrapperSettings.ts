4bb14f4c6b35caa4955d5b77bdcd4b55b7880ed9
John Carmichael <john@johnine.org>
2020-03-03 13:03:26 +0100

    docs: gh-pages initial commit

--- src/models/QWrapperSettings.ts
+++
@@ -1,9 +0,0 @@
-import {ConnectionOptions} from "./ConnectionOptions";
-
-export interface QWrapperSettings {
-  connection: string | ConnectionOptions;
-  queue: string;
-  dleQueue: string;
-  exchange: string;
-  exchangeType?: string;
-}
--- src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export {ConnectionOptions} from "./ConnectionOptions";
-export {ConsumerResponse} from "./ConsumerResponse";
-export {Message} from "./Message";
-export {QWrapperSettings} from "./QWrapperSettings";
