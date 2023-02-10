be2ab4b1435add8e3fc42896bf0af7d21a36c47b
Santiago Sanchez <sanchez@acrontum.de>
2023-02-10 08:47:39 +0100

    Feature/routing keys (#29)
    * add custom routing key option
    
    * bump version
    
    ---------
    
    Co-authored-by: Santiago Sanchez <ssd170@gmail.com>

--- src/models/QWrapperSettings.ts
+++ src/models/QWrapperSettings.ts
@@ -1,14 +1,21 @@
 import { ConnectionOptions } from './ConnectionOptions';
 
+export enum ExchangeType {
+  direct = 'direct',
+  topic = 'topic',
+  fanout = 'fanout',
+}
+
 export interface QWrapperSettings {
   connection: string | ConnectionOptions;
-  queue: string;
+  dleExchange?: string;
+  dleQueue: string;
   exchange: string;
-  exchangeType: string;
+  exchangeType: ExchangeType | string;
+  prefetch?: number;
+  queue: string;
+  reconnect?: boolean;
+  routingKey?: string;
   verboseLogging?: boolean;
   veryVerboseLogging?: boolean;
-  reconnect?: boolean;
-  prefetch?: number;
-  dleQueue: string;
-  dleExchange?: string;
 }
